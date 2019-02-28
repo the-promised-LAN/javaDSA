a1a413757bb60caa21fc1fd66325debe6ae6f2b1
the-promised-LAN <k.vas97@yahoo.com>
2019-02-28 17:39:20 -0500

    inital commit

---
+++ src/javaDSA/Sorts.java
@@ -0,0 +1,55 @@
+package javaDSA;
+
+/**
+ * Static class containing different sorting algorithms for
+ * integer arrays (can be adapted to other data structures)
+ * Includes:
+ * - Quick Sort
+ * - Merge Sort
+ * - Insertion Sort
+ * - Selection Sort
+ * - Radix Sort
+ * - Bubble Sort
+ * - Heap Sort
+ * @author the-promised-LAN
+ *
+ */
+public class Sorts {
+
+	// TODO
+	public static void quickSort(int arr[], int start, int end) {
+		
+	}
+	
+	// TODO
+	public static void mergeSort(int arr[], int start, int end) {
+		
+	}
+	
+	// TODO
+	public static void insertionSort(int arr[]) {
+		
+	}
+	
+	// TODO
+	public static void selectionSort(int arr[]) {
+		
+	}
+	
+	// TODO
+	public static void radixSort(int arr[]) {
+	
+	}
+	
+	// TODO
+	public static void bubbleSort(int arr[]) {
+		
+	}
+	
+	// TODO
+	public static void heapSort(Heap heap) {
+		
+	}
+	
+	
+}
